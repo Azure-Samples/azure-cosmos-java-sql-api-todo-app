7b5de2c4926e5659bbc4281ab3a450302f8d1163
Sneha Gunda <sngun@microsoft.com>
2021-02-10 17:23:06 -0800

    Updating to latest version and chnaging package name

--- src/com/microsoft/azure/cosmos/sample/controller/TodoItemController.java
+++ src/com/microsoft/azure/cosmos/sample/controller/TodoItemController.java
@@ -1,98 +1,55 @@
 package com.microsoft.azure.cosmos.sample.controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
 import java.util.List;
-import java.util.Random;
+import java.util.UUID;
 
-import org.junit.Before;
-import org.junit.Test;
+import lombok.NonNull;
 
-import com.microsoft.azure.cosmos.sample.dao.DocDbDao;
+import com.microsoft.azure.cosmos.sample.dao.TodoDao;
+import com.microsoft.azure.cosmos.sample.dao.TodoDaoFactory;
 import com.microsoft.azure.cosmos.sample.model.TodoItem;
 
-public class TodoItemControllerTest {
-    private static final String anyTodoItemName = "myName";
-    private static final String anyTodoItemCategory = "myCategory";
-    private static final boolean anyTodoItemComplete = true;
-
-    private TodoItemController todoItemController;
-
-    @Before
-    public void setup() {
-        todoItemController = new TodoItemController(new DocDbDao());
+public class TodoItemController {
+    public static TodoItemController getInstance() {
+        if (todoItemController == null) {
+            todoItemController = new TodoItemController(TodoDaoFactory.getDao());
+        }
+        return todoItemController;
     }
 
-    @Test
-    public void testCreateTodoItem() {
-        // Count how many items are exist in the datastore.
-        List<TodoItem> listOfTodoItems = todoItemController.getTodoItems();
-        int numberOfExistingItems = listOfTodoItems.size();
+    private static TodoItemController todoItemController;
 
-        // Create a random new item.
-        Random randy = new Random();
-        int randomInt = randy.nextInt();
+    private final TodoDao todoDao;
 
-        String testTodoItemId = todoItemController.createTodoItem(
-                anyTodoItemName + randomInt, anyTodoItemCategory + randomInt,
-                randomInt % 2 == 0).getId();
-
-        // Check one of the randomly created items.
-        TodoItem testTodoItem = todoItemController
-                .getTodoItemById(testTodoItemId);
-
-        assertNotNull(testTodoItem);
-        assertEquals(anyTodoItemName + randomInt, testTodoItem.getName());
-        assertEquals(anyTodoItemCategory + randomInt,
-                testTodoItem.getCategory());
-        assertEquals(randomInt % 2 == 0, testTodoItem.isComplete());
-
-        // Check that the total item makes sense.
-        listOfTodoItems = todoItemController.getTodoItems();
-        assertEquals(numberOfExistingItems + 1, listOfTodoItems.size());
+    TodoItemController(TodoDao todoDao) {
+        this.todoDao = todoDao;
     }
 
-    @Test
-    public void testDeleteTodoItem() {
-        // Create an item to delete.
-        String testTodoItemId = todoItemController.createTodoItem(
-                anyTodoItemName, anyTodoItemCategory, anyTodoItemComplete)
-                .getId();
+    public TodoItem createTodoItem(@NonNull String name,
+            @NonNull String category, boolean isComplete) {
+        TodoItem todoItem = new TodoItem();
+        
+        todoItem.setName(name);
+        todoItem.setCategory(category);
+        todoItem.setComplete(isComplete);
+        todoItem.setId(UUID.randomUUID().toString());
 
-        // Sanity Check
-        TodoItem actualTodoItem = todoItemController
-                .getTodoItemById(testTodoItemId);
-        assertNotNull(actualTodoItem);
-
-        // Delete the item.
-        todoItemController.deleteTodoItem(testTodoItemId);
-
-        // Check to see if the item was deleted
-        actualTodoItem = todoItemController.getTodoItemById(testTodoItemId);
-        assertNull(actualTodoItem);
+        return todoDao.createTodoItem(todoItem);
     }
 
-    @Test
-    public void testUpdateTodoItem() {
-        // Create an item to update.
-        boolean oldValue = anyTodoItemComplete;
-        TodoItem testTodoItem = todoItemController.createTodoItem(
-                anyTodoItemName, anyTodoItemCategory, oldValue);
+    public boolean deleteTodoItem(@NonNull String id) {
+        return todoDao.deleteTodoItem(id);
+    }
 
-        // Sanity Check
-        boolean actualValue = todoItemController.getTodoItemById(
-                testTodoItem.getId()).isComplete();
-        assertEquals(oldValue, actualValue);
+    public TodoItem getTodoItemById(@NonNull String id) {
+        return todoDao.readTodoItem(id);
+    }
 
-        // Update whether the item is complete.
-        boolean newValue = !oldValue;
-        todoItemController.updateTodoItem(testTodoItem.getId(), newValue);
+    public List<TodoItem> getTodoItems() {
+        return todoDao.readTodoItems();
+    }
 
-        // Check to see if the value was updated.
-        actualValue = todoItemController.getTodoItemById(testTodoItem.getId())
-                .isComplete();
-        assertEquals(newValue, actualValue);
+    public TodoItem updateTodoItem(@NonNull String id, boolean isComplete) {
+        return todoDao.updateTodoItem(id, isComplete);
     }
 }
